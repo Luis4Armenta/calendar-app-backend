4326059d22213f823f986f8a7c65fb8cf3ee8728
beku86 <luigiangelxd@gmail.com>
2021-09-05 15:53:18 -0500

    authentication

---
+++ middlewares/validate-jwt.js
@@ -0,0 +1,34 @@
+const { response, request } = require("express")
+const jwt = require('jsonwebtoken');
+
+const validateJWT = (req = request, res = response, next) => {
+  const token = req.header('x-token');
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: 'No hay token en la petición'
+    })
+  }
+
+  try {
+    const { uid, name } = jwt.verify(
+      token,
+      process.env.SECRET_JWT_SEED
+    );
+
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return response.status(401).json({
+      ok: false,
+      msg: 'Token no valido'
+    });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateJWT
+}
